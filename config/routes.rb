855dfa23dcabb573c9fd22ca1f72231b7f88cf8d
cpan001 <github email address>
2017-10-17 14:38:34 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   get 'home', to: 'static#home'
   resources :orders
+  resources :products
   resources :invoices
+
+  get "/products/:id/description", to: "products#description", as: "description"
+  get "/products/:id/inventory", to: "products#inventory", as: "inventory"
 end
